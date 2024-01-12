81c7aaeab70866866e425ce267e8469177db994e
Adarsh <Adarshbunny293@gmail.com>
2024-01-13 01:03:54 +0530

    Intial UI with Firebase backend
    Added
    - Google Auth
    - Guest login
    - Home screen
    - Community section
    - Dark Mode/ Light Mode

--- aid_employ/lib/main.dart
+++ aid_employ/lib/main.dart
@@ -1,24 +1,70 @@
-import 'package:flutter/material.dart';
-import 'package:aid_employ/pages/auth_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:aid_employ/core/common/error_text.dart';
+import 'package:aid_employ/core/common/loader.dart';
+import 'package:aid_employ/features/auth/controlller/auth_controller.dart';
+import 'package:aid_employ/firebase_options.dart';
+import 'package:aid_employ/models/user_model.dart';
+import 'package:aid_employ/router.dart';
+import 'package:aid_employ/theme/pallete.dart';
+import 'package:routemaster/routemaster.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
   @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  UserModel? userModel;
+
+
+  void getData(WidgetRef ref, User data) async {
+    userModel = await ref
+        .watch(authControllerProvider.notifier)
+        .getUserData(data.uid)
+        .first;
+    ref.read(userProvider.notifier).update((state) => userModel);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AuthPage(),
-    );
+    return ref.watch(authStateChangeProvider).when(
+          data: (data) => MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            title: 'Aid Employ App',
+            theme: ref.watch(themeNotifierProvider),
+            routerDelegate: RoutemasterDelegate(
+              routesBuilder: (context) {
+                if (data != null) {
+                  getData(ref, data);
+                  if (ref.watch(userProvider)!= null) {
+                    return loggedInRoute;
+                  }
+                }
+                print('Returning loggedOutRoute');
+                return loggedOutRoute;
+              },
+            ),
+            routeInformationParser: const RoutemasterParser(),
+          ),
+          error: (error, stackTrace) => ErrorText(error: error.toString()),
+          loading: () => const Loader(),
+        );
   }
 }
--- aid_employ/windows/runner/main.cpp
+++ aid_employ/windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"aid_employ", origin, size)) {
+  if (!window.CreateAndShow(L"aid_employ", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
