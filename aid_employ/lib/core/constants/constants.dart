ead659d422747960c2bd41894f2048a0a405e281
Adarsh <Adarshbunny293@gmail.com>
2024-02-23 22:41:00 +0530

    [updated] Default Avatar
    [updated] Default Avatar for users and communities

--- aid_employ/lib/core/constants/constants.dart
+++ aid_employ/lib/core/constants/constants.dart
@@ -11,7 +11,7 @@ class Constants {
   static const bannerDefault =
       'https://thumbs.dreamstime.com/b/abstract-stained-pattern-rectangle-background-blue-sky-over-fiery-red-orange-color-modern-painting-art-watercolor-effe-texture-123047399.jpg';
   static const avatarDefault =
-      'https://external-preview.redd.it/5kh5OreeLd85QsqYO1Xz_4XSLYwZntfjqou-8fyBFoE.png?auto=webp&s=dbdabd04c399ce9c761ff899f5d38656d1de87c2';
+      'https://firebasestorage.googleapis.com/v0/b/soln-no-poverty.appspot.com/o/Defaults%2FAvatar.png?alt=media&token=15219f88-1e6b-4b7c-8601-081c1930d061';
 
   static const tabWidgets = [
     FeedScreen(),
