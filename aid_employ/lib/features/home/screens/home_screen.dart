daa3fc4cc6190e91a08e5aff420a03827136b946
Adarsh <Adarshbunny293@gmail.com>
2024-03-03 11:15:03 +0530

    [UI] Removed Redundant "Add post" button from Home Screen

--- aid_employ/lib/features/home/screens/home_screen.dart
+++ aid_employ/lib/features/home/screens/home_screen.dart
@@ -65,12 +65,6 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
             },
             icon: const Icon(Icons.search),
           ),
-          IconButton(
-            onPressed: () {
-              Routemaster.of(context).push('/add-post');
-            },
-            icon: const Icon(Icons.add),
-          ),
           Builder(builder: (context) {
             return IconButton(
               icon: CircleAvatar(
