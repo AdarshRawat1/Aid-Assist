c136c774f33ff8a975017b59d10dff921c10fb58
Adarsh <Adarshbunny293@gmail.com>
2024-04-16 23:10:11 +0530

    Fix : Community NULL bug

--- aid_employ/lib/features/home/drawers/community_list_drawer.dart
+++ aid_employ/lib/features/home/drawers/community_list_drawer.dart
@@ -16,7 +16,7 @@ class CommunityListDrawer extends ConsumerWidget {
   }
 
   void navigateToCommunity(BuildContext context, Community community) {
-    Routemaster.of(context).push('/Aid/${community.name}');
+    Routemaster.of(context).push('/r/${community.name}');
   }
 
   @override
