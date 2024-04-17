66925737f4b3d4863df69dcd38bc1fcd5ce3b0b3
Adarsh <Adarshbunny293@gmail.com>
2024-04-17 12:36:21 +0530

    [Major]Initial Working Configuration of Donation Map

--- aid_employ/lib/features/donate/donate_map.dart
+++ aid_employ/lib/features/donate/donate_map.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class DonationScreen extends StatefulWidget {
-  const DonationScreen({super.key});
-
+   const DonationScreen({super.key});
   @override
-  State<DonationScreen> createState() => _DonationScreen();
+  State<DonationScreen> createState() => _DonationScreenState();
 }
 
-class _DonationScreen extends State<DonationScreen> {
-  static const googlePlex = LatLng(30.2870, 77.9983);
+
+class _DonationScreenState extends State<DonationScreen> {
+  static const googlePlex = LatLng(30.2690, 77.9916);
+  
   @override
   void initState() {
     super.initState();
   }
-
+  
   @override
-  Widget build(BuildContext context) =>const Scaffold(
+   // ignore: prefer_const_constructors
+   Widget build(BuildContext context)=> const Scaffold(
       body: GoogleMap(
-          initialCameraPosition: CameraPosition(
-            target: googlePlex, 
-            zoom: 13
-            )
-          )
-      );
-}
+        initialCameraPosition: CameraPosition(
+          target: googlePlex,
+          zoom: 17,
+        ),
+      )
+    );
+   }
+  
+  
+
+
